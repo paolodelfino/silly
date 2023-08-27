c5e5c0f06c38e7375783299a3283f1ed1a32f5a6
paolodelfino <hackymail12@gmail.com>
2023-08-27 05:43:54 -0700

    Add cn function

--- src/app/_lib/utils.ts
+++ src/app/_lib/utils.ts
@@ -1,6 +1,12 @@
 import { env } from "@/env.mjs";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export const domain = () =>
   env.NODE_ENV == "development"
     ? "http://localhost:3000"
     : "https://sillymovie.vercel.app";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
