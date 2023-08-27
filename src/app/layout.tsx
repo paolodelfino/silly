ee7087a3b8da49f736275cf436a1fb861a1d7965
paolodelfino <hackymail12@gmail.com>
2023-08-27 05:45:06 -0700

    Remove useless code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
+import Provider from "@/app/_trpc/Provider";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "@/app/_trpc/Provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         <Theme appearance="dark">
           <Provider>{children}</Provider>
         </Theme>
