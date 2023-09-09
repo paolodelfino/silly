51ce191153e00f43e9e00bdeded16c048486f916
paolodelfino <hackymail12@gmail.com>
2023-09-09 10:49:52 -0700

    Add shortcuts for videoplayer

--- src/app/_components/VideoPlayer.tsx
+++ src/app/_components/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useDocumentTitle } from "@mantine/hooks";
+import { useDocumentTitle, useHotkeys } from "@mantine/hooks";
 import Hls from "hls.js";
 import { useEffect, useRef } from "react";
 
@@ -31,14 +31,78 @@ export default function VideoPlayer({
       } else {
         video.current.src = playlist;
       }
+
+      video.current.addEventListener("seeking", (e) => e.preventDefault());
     }
   }, [video, playlist]);
 
+  useHotkeys([
+    [
+      "ArrowLeft",
+      () => {
+        if (video.current) {
+          video.current.currentTime -= 5;
+        }
+      },
+    ],
+    [
+      "ArrowRight",
+      () => {
+        if (video.current) {
+          video.current.currentTime += 5;
+        }
+      },
+    ],
+    [
+      "ArrowUp",
+      () => {
+        if (video.current && video.current.volume <= 0.9) {
+          video.current.volume += 0.1;
+        }
+      },
+    ],
+    [
+      "ArrowDown",
+      () => {
+        if (video.current && video.current.volume >= 0.1) {
+          video.current.volume -= 0.1;
+        }
+      },
+    ],
+    [
+      "f",
+      () => {
+        if (video.current) {
+          document.fullscreenElement
+            ? document.exitFullscreen()
+            : video.current.requestFullscreen();
+        }
+      },
+    ],
+    [
+      "space",
+      () => {
+        if (video.current) {
+          video.current.paused ? video.current.play() : video.current.pause();
+        }
+      },
+    ],
+    [
+      "k",
+      () => {
+        if (video.current) {
+          video.current.paused ? video.current.play() : video.current.pause();
+        }
+      },
+    ],
+  ]);
+
   return (
     <video
+      onKeyDown={(e) => e.preventDefault()}
       ref={video}
       controls
-      className="w-full aspect-video border-divider border rounded-medium"
+      className="w-full outline-none aspect-video border-divider border rounded-medium"
     ></video>
   );
 }
