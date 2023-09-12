1e6ee2cd4314a7768723b24ffb479753163b2f58
paolodelfino <hackymail12@gmail.com>
2023-09-12 12:50:00 -0700

    Add currentUser utility

--- src/app/_lib/auth.ts
+++ src/app/_lib/auth.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db";
+import { users } from "@/db/schema";
 import { env } from "@/env.mjs";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { eq } from "drizzle-orm";
 import { AuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -25,4 +27,11 @@ export const authOptions: AuthOptions = {
 
 export function auth() {
   return getServerSession(authOptions);
+}
+
+/**
+ * @description id param is supposed to be valid
+ */
+export async function currentUser(id: string) {
+  return (await db.select().from(users).where(eq(users.id, id))).at(0)!;
 }
