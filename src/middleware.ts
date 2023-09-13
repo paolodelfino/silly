192b599f0c16a1b37403a11dc8dbf51612465d4a
paolodelfino <hackymail12@gmail.com>
2023-09-13 08:33:42 -0700

    Fix the method to check authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,10 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-  });
-  if (!token) {
+  if (!request.cookies.get("next-auth.session-token")) {
     return NextResponse.redirect(new URL(`/signin`, request.url));
   }
-  return NextResponse.next(request);
+  return NextResponse.next();
 }
 
 export const config = { matcher: ["/mylist"] };
