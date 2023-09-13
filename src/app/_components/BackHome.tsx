41b48dd18b3275dda46f5e526bfdee48151dfd90
paolodelfino <hackymail12@gmail.com>
2023-09-13 13:30:59 -0700

    Add customization

--- src/app/_components/BackHome.tsx
+++ src/app/_components/BackHome.tsx
@@ -1,10 +1,29 @@
 "use client";
 import { useHotkeys } from "@mantine/hooks";
-import { Button, cn } from "@nextui-org/react";
+import { Button, button, cn } from "@nextui-org/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { ReactNode } from "react";
 
-export default function BackHome({ className }: { className?: string }) {
+export default function BackHome({
+  className,
+  icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"></path>
+    </svg>
+  ),
+  buttonProps,
+}: {
+  className?: string;
+  icon?: ReactNode;
+  buttonProps?: ReturnType<typeof button>;
+}) {
   const pathname = usePathname();
   const router = useRouter();
 
@@ -20,19 +39,12 @@ export default function BackHome({ className }: { className?: string }) {
       className={cn(
         "p-1.5 !opacity-80",
         className,
+        buttonProps,
         pathname == "/" && "hidden"
       )}
       aria-label="Back"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-      >
-        <path d="M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"></path>
-      </svg>
+      {icon}
     </Button>
   );
 }
