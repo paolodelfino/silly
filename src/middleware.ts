8e6863f044550b81d3c7a288191147809b2ce0d3
paolodelfino <hackymail12@gmail.com>
2023-09-13 08:34:47 -0700

    Append callbackUrl to signin page redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,9 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
   if (!request.cookies.get("next-auth.session-token")) {
-    return NextResponse.redirect(new URL(`/signin`, request.url));
+    return NextResponse.redirect(
+      new URL(`/signin?callbackUrl=${request.url}`, request.url)
+    );
   }
   return NextResponse.next();
 }
