ed09a24b747447028e14cf9106828b399d7a2f90
paolodelfino <hackymail12@gmail.com>
2023-09-14 06:20:11 -0700

    Update model

--- src/app/_components/MediaSliderSkeleton.tsx
+++ src/app/_components/MediaSliderSkeleton.tsx
@@ -1,10 +1,21 @@
 import { Skeleton } from "@nextui-org/react";
 
-export default function MediaSliderSkeleton({ seeAll }: { seeAll?: boolean }) {
+export default function MediaSliderSkeleton({
+  titleWidth,
+  seeAll,
+}: {
+  titleWidth: number;
+  seeAll?: boolean;
+}) {
   return (
-    <div className="gap-1 flex flex-col px-1">
+    <div className="space-y-1 flex flex-col px-1">
       <div className="flex justify-between h-10">
-        <Skeleton className="w-[50px] rounded-large" />
+        <Skeleton
+          className="rounded-large"
+          style={{
+            width: titleWidth + "px",
+          }}
+        />
         {seeAll && <Skeleton className="w-[80px] rounded-large" />}
       </div>
 
