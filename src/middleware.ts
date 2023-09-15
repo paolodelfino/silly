58146b8c07161fcadcc2915684ecf75dd43b8830
paolodelfino <hackymail12@gmail.com>
2023-09-15 14:44:43 -0700

    Add check for secure next auth session token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  if (!request.cookies.get("next-auth.session-token")) {
+  if (
+    !request.cookies.get("next-auth.session-token") ||
+    !request.cookies.get("__Secure-next-auth.session-token")
+  ) {
     return NextResponse.redirect(
       new URL(`/signin?callbackUrl=${request.url}`, request.url)
     );
