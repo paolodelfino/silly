eb89ed8859fbf380a173add1ca2d93c20e1358c6
paolodelfino <hackymail12@gmail.com>
2023-09-16 08:50:45 -0700

    Add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Spinner } from "@nextui-org/react";
+
+export default function LoadingPage() {
+  return (
+    <center className="pt-12">
+      <Spinner />
+    </center>
+  );
+}
