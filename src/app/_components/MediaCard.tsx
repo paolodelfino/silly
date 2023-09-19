82039b55576bfbaaa7cf691c14c382542cf2240a
paolodelfino <hackymail12@gmail.com>
2023-09-19 07:19:00 -0700

    Wrap card into div to prevent actions to activate href

--- src/app/_components/MediaCard.tsx
+++ src/app/_components/MediaCard.tsx
@@ -13,38 +13,40 @@ export default function MediaCard<T extends GenericMedia>({
   actions?: MediaSliderProps<T>["actions"];
 }) {
   return (
-    <Card
-      as={Link}
-      href={`/display/${type}/${entry.id}`}
-      className="!outline-non h-[180px] w-[120px] data-[focus-visible=true]:scale-[0.95]"
-      isPressable
-      isHoverable
-    >
-      <CardHeader className="absolute left-0 top-0 z-10 h-full w-full !items-center justify-center">
-        {!entry.poster_path && !entry.profile_path && (
-          <span className="line-clamp-3 text-center">
-            {entry.title ?? entry.name}
-          </span>
+    <div className="relative">
+      <Card
+        as={Link}
+        href={`/display/${type}/${entry.id}`}
+        className="!outline-non h-[180px] w-[120px] data-[focus-visible=true]:scale-[0.95]"
+        isPressable
+        isHoverable
+      >
+        <CardHeader className="absolute left-0 top-0 z-10 h-full w-full !items-center justify-center">
+          {!entry.poster_path && !entry.profile_path && (
+            <span className="line-clamp-3 text-center">
+              {entry.title ?? entry.name}
+            </span>
+          )}
+        </CardHeader>
+
+        {(entry.poster_path || entry.profile_path) && (
+          <Image
+            removeWrapper
+            as={NextImage}
+            radius="none"
+            loading="eager"
+            width={120}
+            height={180}
+            src={`https://image.tmdb.org/t/p/original/${
+              entry.poster_path || entry.profile_path
+            }`}
+            alt={entry.title ?? entry.name!}
+            className="z-0 h-full w-full object-cover"
+          />
         )}
-      </CardHeader>
+      </Card>
 
       {actions?.({ entry })}
-
-      {(entry.poster_path || entry.profile_path) && (
-        <Image
-          removeWrapper
-          as={NextImage}
-          radius="none"
-          loading="eager"
-          width={120}
-          height={180}
-          src={`https://image.tmdb.org/t/p/original/${
-            entry.poster_path || entry.profile_path
-          }`}
-          alt={entry.title ?? entry.name!}
-          className="z-0 h-full w-full object-cover"
-        />
-      )}
-    </Card>
+    </div>
   );
 }
