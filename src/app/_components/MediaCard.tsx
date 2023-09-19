690643a3312c5e86b76e0bf89002e046d7e5bf9c
paolodelfino <hackymail12@gmail.com>
2023-09-19 07:03:54 -0700

    Factor out media card from MediaSlider

---
+++ src/app/_components/MediaCard.tsx
@@ -0,0 +1,50 @@
+import { GenericMedia, MediaSliderProps } from "@/app/_components/MediaSlider";
+import { Card, CardHeader, Image } from "@nextui-org/react";
+import NextImage from "next/image";
+import Link from "next/link";
+
+export default function MediaCard<T extends GenericMedia>({
+  entry,
+  type,
+  actions,
+}: {
+  entry: MediaSliderProps<T>["data"]["results"][number];
+  type: string;
+  actions?: MediaSliderProps<T>["actions"];
+}) {
+  return (
+    <Card
+      as={Link}
+      href={`/display/${type}/${entry.id}`}
+      className="!outline-non h-[180px] w-[120px] data-[focus-visible=true]:scale-[0.95]"
+      isPressable
+      isHoverable
+    >
+      <CardHeader className="absolute left-0 top-0 z-10 h-full w-full !items-center justify-center">
+        {!entry.poster_path && !entry.profile_path && (
+          <span className="line-clamp-3 text-center">
+            {entry.title ?? entry.name}
+          </span>
+        )}
+      </CardHeader>
+
+      {actions?.({ entry })}
+
+      {(entry.poster_path || entry.profile_path) && (
+        <Image
+          removeWrapper
+          as={NextImage}
+          radius="none"
+          loading="eager"
+          width={120}
+          height={180}
+          src={`https://image.tmdb.org/t/p/original/${
+            entry.poster_path || entry.profile_path
+          }`}
+          alt={entry.title ?? entry.name!}
+          className="z-0 h-full w-full object-cover"
+        />
+      )}
+    </Card>
+  );
+}
--- src/app/_components/MediaSlider.tsx
+++ src/app/_components/MediaSlider.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Button, Card, CardHeader, Image, Skeleton } from "@nextui-org/react";
-import NextImage from "next/image";
+import MediaCard from "@/app/_components/MediaCard";
+import { Button, Skeleton } from "@nextui-org/react";
 import Link from "next/link";
 import { ReactNode } from "react";
 import "swiper/css";
@@ -36,46 +36,38 @@ export function MediaSliderSkeleton({
   );
 }
 
-export default function MediaSlider<
-  T extends {
-    page: number;
-    results: {
-      id: number;
-      poster_path?: string | null;
-      profile_path?: string | null;
-      release_date?: string;
-      title?: string;
-      vote_count?: number;
-      popularity: number;
-      first_air_date?: string;
-      name?: string;
-    }[];
-    total_pages: number;
-    total_results: number;
-  },
->({
+export type GenericMedia = {
+  page: number;
+  results: {
+    id: number;
+    poster_path?: string | null;
+    profile_path?: string | null;
+    release_date?: string;
+    title?: string;
+    vote_count?: number;
+    popularity: number;
+    first_air_date?: string;
+    name?: string;
+  }[];
+  total_pages: number;
+  total_results: number;
+};
+
+export type MediaSliderProps<T extends GenericMedia> = {
+  title: string;
+  seeAll?: string;
+  data: T;
+  actions?: (props: { entry: T["results"][number] }) => ReactNode;
+  Card?: typeof MediaCard<T>;
+};
+
+export default function MediaSlider<T extends GenericMedia>({
   title,
   seeAll,
   data,
   actions,
-}: {
-  title: string;
-  seeAll?: string;
-  data: T;
-  actions?: (props: {
-    entry: {
-      id: number;
-      poster_path?: string | null;
-      profile_path?: string | null;
-      release_date?: string;
-      title?: string;
-      vote_count?: number;
-      popularity: number;
-      first_air_date?: string;
-      name?: string;
-    };
-  }) => ReactNode;
-}) {
+  Card = MediaCard,
+}: MediaSliderProps<T>) {
   data.results = data.results.filter((entry) => {
     if ((entry.vote_count && entry.vote_count < 40) || entry.popularity < 10) {
       return false;
@@ -125,39 +117,7 @@ export default function MediaSlider<
               key={`${title}-${i}-${entry.id}`}
               className="!w-max first:!ml-2 last:!mr-1"
             >
-              <Card
-                as={Link}
-                href={`/display/${type}/${entry.id}`}
-                className="h-[180px] w-[120px] !outline-none data-[focus-visible=true]:scale-[0.97]"
-                isPressable
-                isHoverable
-              >
-                <CardHeader className="absolute left-0 top-0 z-10 h-full w-full !items-center justify-center">
-                  {!entry.poster_path && !entry.profile_path && (
-                    <span className="line-clamp-3 text-center">
-                      {entry.title ?? entry.name}
-                    </span>
-                  )}
-                </CardHeader>
-
-                {actions?.({ entry })}
-
-                {(entry.poster_path || entry.profile_path) && (
-                  <Image
-                    removeWrapper
-                    as={NextImage}
-                    radius="none"
-                    loading="eager"
-                    width={120}
-                    height={180}
-                    src={`https://image.tmdb.org/t/p/original/${
-                      entry.poster_path || entry.profile_path
-                    }`}
-                    alt={entry.title ?? entry.name!}
-                    className="z-0 h-full w-full object-cover"
-                  />
-                )}
-              </Card>
+              <Card entry={entry} type={type} actions={actions} />
             </SwiperSlide>
           );
         })}
