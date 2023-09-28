a025a0110e333a181676fbe0cb21b1d9efd32d64
paolodelfino <hackymail12@gmail.com>
2023-09-28 07:41:24 -0700

    Add scroll to top

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useHotkeys } from "@mantine/hooks";
+import { useHotkeys, useScrollIntoView, useWindowScroll } from "@mantine/hooks";
 import {
   Avatar,
   Button,
   Dropdown,
   DropdownItem,
   DropdownMenu,
+  DropdownSection,
   DropdownTrigger,
-  Kbd,
   Skeleton,
 } from "@nextui-org/react";
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -16,8 +16,17 @@ import { useState } from "react";
 export default function TopNav() {
   const { data, status } = useSession();
 
-  const [actionsOpen, setActionsOpen] = useState(false);
+  const [{ y }] = useWindowScroll();
+  const { scrollIntoView } = useScrollIntoView({
+    easing(t) {
+      return 1 - Math.pow(1 - t, 4);
+    },
+  });
 
+  const [actionsOpen, setActionsOpen] = useState(false);
+  const menuActions: Record<string, VoidFunction> = {
+    "scroll-to-top": scrollIntoView,
+  };
   useHotkeys([["ctrl+i", () => setActionsOpen(!actionsOpen)]]);
 
   return (
@@ -76,42 +85,67 @@ export default function TopNav() {
           <DropdownMenu
             aria-label="Actions"
             variant="flat"
-            disabledKeys={["settings", "shortcuts"]}
+            disabledKeys={[
+              "settings",
+              "shortcuts",
+              y == 0 ? "scroll-to-top" : "",
+            ]}
+            onAction={(key) => menuActions[key.toString()]()}
           >
-            <DropdownItem
-              key={"shortcuts"}
-              shortcut="^H"
-              startContent={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24"
-                  viewBox="0 -960 960 960"
-                  width="24"
-                  fill="currentColor"
-                >
-                  <path d="M480-280q17 0 28.5-11.5T520-320v-160q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480v160q0 17 11.5 28.5T480-280Zm0-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
-                </svg>
-              }
-            >
-              Shortcuts Help
-            </DropdownItem>
-            <DropdownItem
-              key="settings"
-              shortcut="^S"
-              startContent={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24"
-                  viewBox="0 -960 960 960"
-                  width="24"
-                  fill="currentColor"
-                >
-                  <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
-                </svg>
-              }
-            >
-              Settings
-            </DropdownItem>
+            <DropdownSection showDivider>
+              <DropdownItem
+                key={"shortcuts"}
+                shortcut="^H"
+                startContent={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24"
+                    fill="currentColor"
+                  >
+                    <path d="M480-280q17 0 28.5-11.5T520-320v-160q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480v160q0 17 11.5 28.5T480-280Zm0-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
+                  </svg>
+                }
+              >
+                Shortcuts Help
+              </DropdownItem>
+              <DropdownItem
+                key="settings"
+                shortcut="^S"
+                startContent={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24"
+                    fill="currentColor"
+                  >
+                    <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
+                  </svg>
+                }
+              >
+                Settings
+              </DropdownItem>
+            </DropdownSection>
+            <DropdownSection title={"Miscellaneous"}>
+              <DropdownItem
+                key="scroll-to-top"
+                startContent={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24"
+                    fill="currentColor"
+                  >
+                    <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
+                  </svg>
+                }
+              >
+                Scroll To Top
+              </DropdownItem>
+            </DropdownSection>
           </DropdownMenu>
         </Dropdown>
       </div>
