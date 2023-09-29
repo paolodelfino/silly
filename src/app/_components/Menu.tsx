161bdd5a4eb15ca478015cfb5c12c069d69b28f5
paolodelfino <hackymail12@gmail.com>
2023-09-29 09:36:30 -0700

    Add menu

---
+++ src/app/_components/Menu.tsx
@@ -0,0 +1,18 @@
+import Drawer from "@/app/_components/Drawer";
+import { ReactNode } from "react";
+
+export default function Menu({
+  Trigger,
+  Content,
+}: {
+  Trigger: ReactNode;
+  Content: ReactNode;
+}) {
+  return (
+    <Drawer
+      Trigger={Trigger}
+      vaulContentClassName="h-max p-4 !bg-background/90 after:backdrop-blur backdrop-blur border-t border-divider"
+      Content={Content}
+    />
+  );
+}
