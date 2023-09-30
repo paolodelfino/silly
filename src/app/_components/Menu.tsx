8b8e0fa47aa8ad46fe231cd12e2bbcceec4a99ee
paolodelfino <hackymail12@gmail.com>
2023-09-30 09:34:54 -0700

    Replace vaul drawer with modal due to some issues

--- src/app/_components/Menu.tsx
+++ src/app/_components/Menu.tsx
@@ -1,18 +1,30 @@
-import Drawer from "@/app/_components/Drawer";
+import { Modal, ModalContent, useDisclosure } from "@nextui-org/react";
 import { ReactNode } from "react";
 
 export default function Menu({
   Trigger,
   Content,
 }: {
-  Trigger: ReactNode;
-  Content: ReactNode;
+  Trigger: (modal: ReturnType<typeof useDisclosure>) => ReactNode;
+  Content: (modal: ReturnType<typeof useDisclosure>) => ReactNode;
 }) {
+  const modal = useDisclosure();
+
   return (
-    <Drawer
-      Trigger={Trigger}
-      vaulContentClassName="h-max p-4 min-h-[250px] !bg-background/90 after:backdrop-blur backdrop-blur border-t border-divider"
-      Content={Content}
-    />
+    <>
+      {Trigger(modal)}
+      <Modal
+        isOpen={modal.isOpen}
+        onOpenChange={modal.onOpenChange}
+        classNames={{
+          wrapper: "!items-end",
+        }}
+        scrollBehavior="outside"
+      >
+        <ModalContent className="!m-0 h-max max-h-[96%] min-h-[250px] !w-full !max-w-full rounded-b-none border-t border-divider !bg-background/90 p-4 backdrop-blur after:backdrop-blur">
+          {Content(modal)}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
