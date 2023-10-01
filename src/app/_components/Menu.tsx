651205b4dfa54265280b88acd3ce88c377b40118
paolodelfino <hackymail12@gmail.com>
2023-10-01 01:58:42 -0700

    Add drag functionality

--- src/app/_components/Menu.tsx
+++ src/app/_components/Menu.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { Modal, ModalContent, useDisclosure } from "@nextui-org/react";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export default function Menu({
   Trigger,
@@ -9,6 +10,25 @@ export default function Menu({
   Content: (modal: ReturnType<typeof useDisclosure>) => ReactNode;
 }) {
   const modal = useDisclosure();
+  const [isDragging, setIsDragging] = useState(false);
+  const [yOnPointerDown, setYOnPointerDown] = useState<number | undefined>();
+  const [yOnMove, setYOnPointerMove] = useState<number | undefined>();
+
+  const stopDragging = () => {
+    setIsDragging(false);
+    setYOnPointerMove(undefined);
+    setYOnPointerDown(undefined);
+  };
+
+  useEffect(() => {
+    if (
+      yOnMove != undefined &&
+      yOnPointerDown != undefined &&
+      yOnMove < yOnPointerDown
+    ) {
+      stopDragging();
+    }
+  }, [yOnMove, yOnPointerDown]);
 
   return (
     <>
@@ -19,9 +39,38 @@ export default function Menu({
         classNames={{
           wrapper: "!items-end",
         }}
-        scrollBehavior="outside"
       >
-        <ModalContent className="!m-0 h-max max-h-[96%] min-h-[250px] !w-full !max-w-full rounded-b-none border-t border-divider !bg-background/90 p-4 backdrop-blur after:backdrop-blur">
+        <ModalContent
+          onPointerDown={(e) => {
+            if (e.target != e.currentTarget) return;
+
+            const shiftY =
+              e.clientY - e.currentTarget.getBoundingClientRect().top;
+            setYOnPointerDown(shiftY);
+
+            setIsDragging(true);
+
+            document.onpointerup = () => {
+              stopDragging();
+              document.onpointerup = null;
+            };
+          }}
+          onPointerUp={(e) => {
+            stopDragging();
+          }}
+          onPointerMove={(e) => {
+            if (isDragging) {
+              const shiftY =
+                e.clientY - e.currentTarget.getBoundingClientRect().top;
+              setYOnPointerMove(shiftY);
+
+              if ((shiftY / e.currentTarget.clientHeight) * 100 > 25) {
+                modal.onClose();
+              }
+            }
+          }}
+          className="relative !m-0 h-max max-h-[96%] min-h-[250px] !w-full !max-w-full rounded-b-none border-t border-divider !bg-background/90 p-4 backdrop-blur after:absolute after:left-0 after:right-0 after:top-full after:h-[calc(100vh+100%)] after:w-screen after:bg-inherit after:backdrop-blur"
+        >
           {Content(modal)}
         </ModalContent>
       </Modal>
