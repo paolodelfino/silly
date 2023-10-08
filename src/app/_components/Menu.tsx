295baf9ec76fe4a8ba4bbf7bb8ff06689d2c20c6
paolodelfino <hackymail12@gmail.com>
2023-10-08 01:49:14 -0700

    Reuse vaul drawer and remove scroll to top from menu

--- src/app/_components/Menu.tsx
+++ src/app/_components/Menu.tsx
@@ -1,79 +1,19 @@
-"use client";
-import { Modal, ModalContent, useDisclosure } from "@nextui-org/react";
-import { ReactNode, useEffect, useState } from "react";
+import Drawer from "@/app/_components/Drawer";
+import { ReactNode } from "react";
+import { Drawer as VaulDrawer } from "vaul";
 
 export default function Menu({
   Trigger,
   Content,
 }: {
-  Trigger: (modal: ReturnType<typeof useDisclosure>) => ReactNode;
-  Content: (modal: ReturnType<typeof useDisclosure>) => ReactNode;
+  Trigger: (Vaul: typeof VaulDrawer) => ReactNode;
+  Content: (Vaul: typeof VaulDrawer) => ReactNode;
 }) {
-  const modal = useDisclosure();
-  const [isDragging, setIsDragging] = useState(false);
-  const [yOnPointerDown, setYOnPointerDown] = useState<number | undefined>();
-  const [yOnMove, setYOnPointerMove] = useState<number | undefined>();
-
-  const stopDragging = () => {
-    setIsDragging(false);
-    setYOnPointerMove(undefined);
-    setYOnPointerDown(undefined);
-  };
-
-  useEffect(() => {
-    if (
-      yOnMove != undefined &&
-      yOnPointerDown != undefined &&
-      yOnMove < yOnPointerDown
-    ) {
-      stopDragging();
-    }
-  }, [yOnMove, yOnPointerDown]);
-
   return (
-    <>
-      {Trigger(modal)}
-      <Modal
-        isOpen={modal.isOpen}
-        onOpenChange={modal.onOpenChange}
-        classNames={{
-          wrapper: "!items-end",
-        }}
-      >
-        <ModalContent
-          onPointerDown={(e) => {
-            if (e.target != e.currentTarget) return;
-
-            const shiftY =
-              e.clientY - e.currentTarget.getBoundingClientRect().top;
-            setYOnPointerDown(shiftY);
-
-            setIsDragging(true);
-
-            document.onpointerup = () => {
-              stopDragging();
-              document.onpointerup = null;
-            };
-          }}
-          onPointerUp={(e) => {
-            stopDragging();
-          }}
-          onPointerMove={(e) => {
-            if (isDragging) {
-              const shiftY =
-                e.clientY - e.currentTarget.getBoundingClientRect().top;
-              setYOnPointerMove(shiftY);
-
-              if ((shiftY / e.currentTarget.clientHeight) * 100 > 25) {
-                modal.onClose();
-              }
-            }
-          }}
-          className="relative !m-0 h-max max-h-[96%] min-h-[250px] !w-full !max-w-full rounded-b-none border-t border-divider !bg-background/90 p-4 backdrop-blur after:absolute after:left-0 after:right-0 after:top-full after:h-[calc(100vh+100%)] after:w-screen after:bg-inherit after:backdrop-blur"
-        >
-          {Content(modal)}
-        </ModalContent>
-      </Modal>
-    </>
+    <Drawer
+      Trigger={({ Vaul }) => Trigger(Vaul)}
+      Content={({ Vaul }) => Content(Vaul)}
+      vaulContentClassName="h-max max-h-[96%] min-h-[250px] border-t border-divider !bg-background/90 p-4 backdrop-blur"
+    />
   );
 }
--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,18 +1,16 @@
 "use client";
 import Menu from "@/app/_components/Menu";
 import { useBrowserInfo } from "@/app/_stores/browser-info";
-import { useHotkeys, useScrollIntoView, useWindowScroll } from "@mantine/hooks";
+import { useHotkeys } from "@mantine/hooks";
 import {
   Avatar,
   Button,
   Dropdown,
   DropdownItem,
   DropdownMenu,
-  DropdownSection,
   DropdownTrigger,
   Listbox,
   ListboxItem,
-  ListboxSection,
   Skeleton,
 } from "@nextui-org/react";
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -22,17 +20,8 @@ export default function TopNav() {
   const { data, status } = useSession();
   const { userAgent } = useBrowserInfo();
 
-  const [{ y }] = useWindowScroll();
-  const { scrollIntoView } = useScrollIntoView({
-    easing(t) {
-      return 1 - Math.pow(1 - t, 4);
-    },
-  });
-
   const [actionsOpen, setActionsOpen] = useState(false);
-  const menuActions: Record<string, VoidFunction> = {
-    "scroll-to-top": scrollIntoView,
-  };
+  const menuActions: Record<string, VoidFunction> = {};
   useHotkeys([["ctrl+i", () => setActionsOpen(!actionsOpen)]]);
 
   return (
@@ -72,69 +61,46 @@ export default function TopNav() {
       <div className="flex">
         {userAgent?.device.type == "mobile" ? (
           <Menu
-            Trigger={({ onOpen }) => (
-              <Button onPress={onOpen} isIconOnly radius="full" variant="ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24"
-                  viewBox="0 -960 960 960"
-                  width="24"
-                  fill="currentColor"
-                >
-                  <path d="M160-240q-17 0-28.5-11.5T120-280q0-17 11.5-28.5T160-320h640q17 0 28.5 11.5T840-280q0 17-11.5 28.5T800-240H160Zm0-200q-17 0-28.5-11.5T120-480q0-17 11.5-28.5T160-520h640q17 0 28.5 11.5T840-480q0 17-11.5 28.5T800-440H160Zm0-200q-17 0-28.5-11.5T120-680q0-17 11.5-28.5T160-720h640q17 0 28.5 11.5T840-680q0 17-11.5 28.5T800-640H160Z" />
-                </svg>
-              </Button>
+            Trigger={(Vaul) => (
+              <Vaul.Trigger asChild>
+                <Button isIconOnly radius="full" variant="ghost">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24"
+                    viewBox="0 -960 960 960"
+                    width="24"
+                    fill="currentColor"
+                  >
+                    <path d="M160-240q-17 0-28.5-11.5T120-280q0-17 11.5-28.5T160-320h640q17 0 28.5 11.5T840-280q0 17-11.5 28.5T800-240H160Zm0-200q-17 0-28.5-11.5T120-480q0-17 11.5-28.5T160-520h640q17 0 28.5 11.5T840-480q0 17-11.5 28.5T800-440H160Zm0-200q-17 0-28.5-11.5T120-680q0-17 11.5-28.5T160-720h640q17 0 28.5 11.5T840-680q0 17-11.5 28.5T800-640H160Z" />
+                  </svg>
+                </Button>
+              </Vaul.Trigger>
             )}
-            Content={({ onClose }) => (
+            Content={(Vaul) => (
               <Listbox
                 aria-label="Actions"
                 variant="flat"
-                disabledKeys={[
-                  "settings",
-                  "shortcuts",
-                  y == 0 ? "scroll-to-top" : "",
-                ]}
+                disabledKeys={["settings"]}
                 onAction={(key) => {
                   menuActions[key.toString()]();
-                  onClose();
                 }}
               >
-                <ListboxSection showDivider>
-                  <ListboxItem
-                    key="settings"
-                    startContent={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 -960 960 960"
-                        width="24"
-                        fill="currentColor"
-                      >
-                        <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
-                      </svg>
-                    }
-                  >
-                    Settings
-                  </ListboxItem>
-                </ListboxSection>
-                <ListboxSection title={"Miscellaneous"}>
-                  <ListboxItem
-                    key="scroll-to-top"
-                    startContent={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 -960 960 960"
-                        width="24"
-                        fill="currentColor"
-                      >
-                        <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
-                      </svg>
-                    }
-                  >
-                    Scroll To Top
-                  </ListboxItem>
-                </ListboxSection>
+                <ListboxItem
+                  key="settings"
+                  startContent={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 -960 960 960"
+                      width="24"
+                      fill="currentColor"
+                    >
+                      <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
+                    </svg>
+                  }
+                >
+                  <Vaul.Close>Settings</Vaul.Close>
+                </ListboxItem>
               </Listbox>
             )}
           />
@@ -161,67 +127,43 @@ export default function TopNav() {
               <DropdownMenu
                 aria-label="Actions"
                 variant="flat"
-                disabledKeys={[
-                  "settings",
-                  "shortcuts",
-                  y == 0 ? "scroll-to-top" : "",
-                ]}
+                disabledKeys={["settings", "shortcuts"]}
                 onAction={(key) => menuActions[key.toString()]()}
               >
-                <DropdownSection showDivider>
-                  <DropdownItem
-                    key={"shortcuts"}
-                    shortcut="^H"
-                    startContent={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 -960 960 960"
-                        width="24"
-                        fill="currentColor"
-                      >
-                        <path d="M480-280q17 0 28.5-11.5T520-320v-160q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480v160q0 17 11.5 28.5T480-280Zm0-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
-                      </svg>
-                    }
-                  >
-                    Shortcuts Help
-                  </DropdownItem>
-                  <DropdownItem
-                    key="settings"
-                    shortcut="^S"
-                    startContent={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 -960 960 960"
-                        width="24"
-                        fill="currentColor"
-                      >
-                        <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
-                      </svg>
-                    }
-                  >
-                    Settings
-                  </DropdownItem>
-                </DropdownSection>
-                <DropdownSection title={"Miscellaneous"}>
-                  <DropdownItem
-                    key="scroll-to-top"
-                    startContent={
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="24"
-                        viewBox="0 -960 960 960"
-                        width="24"
-                        fill="currentColor"
-                      >
-                        <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
-                      </svg>
-                    }
-                  >
-                    Scroll To Top
-                  </DropdownItem>
-                </DropdownSection>
+                <DropdownItem
+                  key={"shortcuts"}
+                  shortcut="^H"
+                  startContent={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 -960 960 960"
+                      width="24"
+                      fill="currentColor"
+                    >
+                      <path d="M480-280q17 0 28.5-11.5T520-320v-160q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480v160q0 17 11.5 28.5T480-280Zm0-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
+                    </svg>
+                  }
+                >
+                  Shortcuts Help
+                </DropdownItem>
+                <DropdownItem
+                  key="settings"
+                  shortcut="^S"
+                  startContent={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 -960 960 960"
+                      width="24"
+                      fill="currentColor"
+                    >
+                      <path d="M200-520q-33 0-56.5-23.5T120-600v-160q0-33 23.5-56.5T200-840h160q33 0 56.5 23.5T440-760v160q0 33-23.5 56.5T360-520H200Zm0 400q-33 0-56.5-23.5T120-200v-160q0-33 23.5-56.5T200-440h160q33 0 56.5 23.5T440-360v160q0 33-23.5 56.5T360-120H200Zm400-400q-33 0-56.5-23.5T520-600v-160q0-33 23.5-56.5T600-840h160q33 0 56.5 23.5T840-760v160q0 33-23.5 56.5T760-520H600Zm0 400q-33 0-56.5-23.5T520-200v-160q0-33 23.5-56.5T600-440h160q33 0 56.5 23.5T840-360v160q0 33-23.5 56.5T760-120H600Z" />
+                    </svg>
+                  }
+                >
+                  Settings
+                </DropdownItem>
               </DropdownMenu>
             </div>
           </Dropdown>
