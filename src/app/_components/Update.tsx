73a671b8cf79e7ae3cd8b9b7d6d09dc9dbe12c26
paolodelfino <paolodelfino9@gmail.com>
2024-03-13 10:04:40 +0000

    Older version on again

--- src/app/_components/Update.tsx
+++ src/app/_components/Update.tsx
@@ -7,16 +7,19 @@ import {
   ModalHeader,
   Spacer,
 } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Update() {
+  const [isOpen, setIsOpen] = useState(true);
+  
   return (
-    <Modal isOpen hideCloseButton>
+    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
       <ModalContent>
         <ModalHeader className="flex flex-col gap-1">
           Update is coming
         </ModalHeader>
         <ModalBody>
-          Silly is getting updated, please stay tuned.
+          Silly is getting updated, please stay tuned. Press 'X' to use the older version.
           <Spacer />
         </ModalBody>
       </ModalContent>
